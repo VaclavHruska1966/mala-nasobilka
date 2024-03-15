3d8cbfe53130271bcd270dee3b709497f07b10b8
vaclavos <vaclav.hruska@student.souepl.cz>
2024-03-15 12:19:43 +0100

    nasobilka

---
+++ nasobilka.py
@@ -0,0 +1,26 @@
+import random
+body = 0
+def nasobeni(a, b):
+    vysledek = a * b
+    return vysledek
+   
+def kontrola(vysledek, vysledek_zak):
+    global body
+    if vysledek == vysledek_zak:
+        print("správně")
+        body +=1
+        print(body)
+    else:
+        print("špatně")
+        body = (body)
+        print(body)
+for nasobilka in range(1,11):
+   
+    x = random.randint(1,10)
+    y = random.randint(1,10)
+    vysledek_zak = int(input(f"{x} * {y}="))
+ 
+    vysledek = nasobeni(x,y)
+    kontrola(vysledek,vysledek_zak)  
+ 
+print("Celkový počet bodů je: ",body)
